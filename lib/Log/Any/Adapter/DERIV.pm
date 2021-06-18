68b782f17d4031d3c5d5d6b2ec4361dff44d92ff
chylli-binary <chylli@binary.com>
2021-06-18 03:04:53 +0000

    eol

--- lib/Log/Any/Adapter/DERIV.pm
+++ lib/Log/Any/Adapter/DERIV.pm
@@ -25,7 +25,7 @@ Log::Any::Adapter::DERIV - standardised logging to STDERR and JSON file
 
     #specify STDERR directly
     use Log::any::Adapter ('DERIV', stderr => 1)
-    
+
     #specify STDERR's format
     use Log::any::Adapter ('DERIV', stderr => 'json')
 
