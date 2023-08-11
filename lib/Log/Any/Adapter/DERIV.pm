e8b9fb4bd65597fe5529a2a4e82428b72e3382fa
aisha-deriv <aisha@deriv.com>
2023-08-11 11:22:21 +0000

    added json mesage to log message - structured logging [ci]

--- lib/Log/Any/Adapter/DERIV.pm
+++ lib/Log/Any/Adapter/DERIV.pm
@@ -11,7 +11,7 @@ use feature qw(state);
 use parent qw(Log::Any::Adapter::Coderef);
 
 use utf8;
-
+use JSON;
 =encoding utf8
 
 =head1 NAME
@@ -358,6 +358,24 @@ sub log_entry {
     my $get_json  = sub { $json_data //= encode_json_text($data) . "\n"; return $json_data; };
     my $get_text =
         sub { my $color = shift // 0; $text_data{$color} //= $self->format_line($data, {color => $color}) . "\n"; return $text_data{$color}; };
+
+    # remove substitution context from message
+    if ($data->{message})
+    {
+        $data->{message} =~ s/\".*//;
+    }  
+    # Prepare the JSON object with the required fields
+    my %log_data = (
+        message   => $data->{message},
+        severity  => $data->{severity},
+    );    
+    if ($self->{context} && ref($self->{context}) eq 'HASH'){
+        foreach my $key (keys %{$self->{context}}) {
+            $log_data{$key} = $self->{context}->{$key};
+        }
+        my $json_string = encode_json(\%log_data);
+        $data->{message} = $json_string;
+    }  
 
     if ($self->{json_fh}) {
         _lock($self->{json_fh});
